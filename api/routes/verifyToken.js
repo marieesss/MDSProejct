be681e0c479c73d688d8c9b675c27694c6115eeb
marieesss <mje.espinosa@gmail.com>
2023-01-31 17:24:14 +0100

    fix verify token

--- api/routes/verifyToken.js
+++ api/routes/verifyToken.js
@@ -15,24 +15,24 @@ const verifyToken =(req, res, next)=>{
         return res.status(401).json("You are not authenticated !")
     }
 }
+const verifyTokenAuth= (req, res, next) => {
+    verifyToken(req, res, () => {
+      if (req.user.id === req.params.id || req.user.isAdmin) {
+        next();
+      } else {
+        res.status(403).json("You are not alowed to do that!");
+      }
+    });
+  };
 
-const verifyTokenAuth=(req, res, next) =>{
-    verifyToken(req,res,()=>{
-        if(req.user.id === req.params.id || req.user.isAdmin){
-            next();
-        }else{
-            response.status(403).json("Not allowed to do that");
-        }
-    })
-}
-
-const verifyTokenAdmin=(req, res, next) =>{
-    verifyToken(req,res,()=>{
-        if(req.user.isAdmin){
-            next();
-        }else{
-            response.status(403).json("Not allowed to do that");
-        }
-    })
-}
+  const verifyTokenAdmin = (req, res, next) => {
+    verifyToken(req, res, () => {
+      if (req.user.isAdmin) {
+        next();
+      } else {
+        res.status(403).json("You are not alowed to do that!");
+      }
+    });
+  };
+  
 module.exports ={ verifyToken, verifyTokenAuth, verifyTokenAdmin};

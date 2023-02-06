168b0f6c78b2d51bcac5e923f5a641b3e3404c8f
marieesss <mje.espinosa@gmail.com>
2023-02-06 20:12:54 +0100

    fix bug

--- api/routes/User.js
+++ api/routes/User.js
@@ -7,41 +7,36 @@ const {
     verifyTokenAdmin,
   } = require("./verifyToken");
 
-
 //UPDATE
+router.put("/:id", verifyTokenAdmin, async (req, res) => {
+  if (req.body.password) {
+    req.body.password = 
+      req.body.password
+  }
 
-router.put("/:id", verifyTokenAuth, async (req, res) => {
-    if (req.body.password) {
-      req.body.password = CryptoJS.AES.encrypt(
-        req.body.password,
-        process.env.PASS_SEC
-      ).toString();
-    }
-  
-    try {
-      const updatedUser = await User.findByIdAndUpdate(
-        req.params.id,
-        {
-          $set: req.body,
-        },
-        { new: true }
-      );
-      res.status(200).json(updatedUser);
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  });
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
-  //DELETE 
-
-  router.delete("/:id", verifyTokenAuth, async (req, res)=>{
-    try{
-        await User.findByIdAndDelete(req.params.id);
-        restart.status(200).json("User has been delected")
-    }catch(err){
-        res.status(500).json.apply(err)
-    }
-  })
+ //DELETE
+router.delete("/:id", verifyTokenAuth, async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).json("User has been deleted...");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 
     //GET 

2a3c49f19fd29abf872f601e89938c72a604bd32
marieesss <mje.espinosa@gmail.com>
2023-02-07 14:16:49 +0100

    fix bug

--- api/routes/Order.js
+++ api/routes/Order.js
@@ -42,26 +42,25 @@ router.put("/:id", verifyTokenAuth, async (req, res) => {
 
   //DELETE 
 
-  router.delete("/:id", verifyTokenAdmin, async (req, res)=>{
-    try{
-        await Order.findByIdAndDelete(req.params.id);
-        restart.status(200).json("Order has been delected")
-    }catch(err){
-        res.status(500).json.apply(err)
+  router.delete("/:id", verifyTokenAdmin, async (req, res) => {
+    try {
+      await Order.findByIdAndDelete(req.params.id);
+      res.status(200).json("Order has been deleted...");
+    } catch (err) {
+      res.status(500).json(err);
     }
-  })
-
+  });
 
     //GET USER Order
 
-    router.get("/find/:userId", verifyTokenAuth, async (req, res)=>{
-        try{
-            const Order = await Order.findById({userId: req.params.userId,});
-            res.status(200).json(Order);
-        }catch(err){
-            res.status(500).json.apply(err)
+    router.get("/find/:userId", verifyTokenAuth, async (req, res) => {
+        try {
+          const orders = await Order.find({ userId: req.params.userId });
+          res.status(200).json(orders);
+        } catch (err) {
+          res.status(500).json(err);
         }
-      })
+      });
 
 
          //GET All Order
@@ -79,27 +78,30 @@ router.get("/", verifyTokenAdmin, async (req, res) => {
 //GET MONTHLY STATS
 
 router.get("/stats", verifyTokenAdmin, async (req, res) => {
-    const date= new Date();
-    const lastMonth = new Date(date.setMonth(date.getMonth() -1));
-    const previousMonth = new Date(date.setMonth(lastMonth.getMonth() -1));
-        try{
-            const stats = await Order.aggregate([
-                {$match: {createdAt: {$gte: previousMonth}}}, 
-                {$projet:{
-                month:{$month: "$createdAT"},
-                sales:"$amount"
-            },
-        $group:{
-            _id:"$month",
-            total:{$sum:"$sales"}
-        }
-        }
-            ]);
-            res.status(200).json(stats);
-
-        }catch(err){
-            res.status(500).json(err);
-        }
-})
-
+    const date = new Date();
+    const lastMonth = new Date(date.setMonth(date.getMonth() - 1));
+    const previousMonth = new Date(new Date().setMonth(lastMonth.getMonth() - 1));
+  
+    try {
+      const income = await Order.aggregate([
+        { $match: { createdAt: { $gte: previousMonth } } },
+        {
+          $project: {
+            month: { $month: "$createdAt" },
+            sales: "$amount",
+          },
+        },
+        {
+          $group: {
+            _id: "$month",
+            total: { $sum: "$sales" },
+          },
+        },
+      ]);
+      res.status(200).json(income);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
 module.exports = router

d265f66eb6060096698bf37562a1c5b32abb5aa1
marieesss <mje.espinosa@gmail.com>
2023-02-22 09:48:46 +0100

    api calls

--- web-app/src/requestMethods.js
+++ web-app/src/requestMethods.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const BASE_URL = "http://localhost:5000/api/"
-const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzZWNkNmM2NzQxZmMxZWJkZDVkNjQ4ZiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY3NjQ2NTkwMiwiZXhwIjoxNjc2NzI1MTAyfQ._WY07h0sJletd3QVFcCpZgULI12iSYfTuQbGElGMZBs"
+const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzZWNkNmM2NzQxZmMxZWJkZDVkNjQ4ZiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY3Njk3NDQ1NywiZXhwIjoxNjc3MjMzNjU3fQ.GfLLfHSWY5fe6-Z19MFKHRaxX2oPPiMFsbs4LceoYNk"
 
 export const publicRequest= axios.create({
     baseURL: BASE_URL,

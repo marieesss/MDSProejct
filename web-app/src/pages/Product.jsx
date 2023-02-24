cd23de645676a207a5370ecc80bd988829677879
marieesss <mje.espinosa@gmail.com>
2023-02-24 10:07:55 +0100

    product

--- web-app/src/pages/Product.jsx
+++ web-app/src/pages/Product.jsx
@@ -4,12 +4,15 @@ import { publicRequest } from "../requestMethods";
 import Menu from '../components/Menu';
 import { addProduct } from '../redux/cartRedux';
 import { useDispatch } from 'react-redux';
+import axios from 'axios';
 
 
 const Product = () => {
   const location = useLocation();
   const id = location.pathname.split('/')[2];
   const [Product, setProduct]=  useState({});
+  const [Fermier, setFermier]=  useState({});
+  const [FermierId, setFermierId]=  useState("");
   const [quantity, setQuantity]=  useState(1);
   const dispatch = useDispatch();
 
@@ -18,12 +21,36 @@ const Product = () => {
         try{
           const res = await publicRequest.get("/product/find/" + id);
           setProduct(res.data)
+          setFermierId(res.data.fermierId)
+          
         }catch{
 
         }
       };
       getProduct();
+     
+    
+      
   }, [id])
+
+
+  useEffect(()=>{
+    console.log(FermierId)
+    const getFermier = async ()=> {
+      try{
+        const res = await axios.get(`http://localhost:5000/api/fermier/find/${FermierId}` );
+  
+        setFermier(res.data)
+      }catch(err){
+        console.log(err)
+      }
+    };
+    getFermier();
+   
+  
+    
+}, [FermierId])
+
 
   const handleQuantity = (type) => {
     if (type === "dec") {
@@ -44,13 +71,16 @@ const Product = () => {
       <p>{Product.title}</p>
       <img src={Product.img}/>
       <p>{Product.desc}</p>
-      <p>{Product.price}</p>
+      <p>{Product.fermierId}</p>
       <button onClick={()=>handleQuantity("inc")}/>
       <p>{quantity}</p>
       <button onClick={()=>handleQuantity("dec")}/>
       <button onClick={()=>handleClick()}>
         Ajouter au panier
       </button>
+      <p>{Fermier.desc}</p>
+      <p>{Fermier.name}</p>
+      <img src={Fermier.img}/>
     </div>
   )
 }

d12196f5757d6adadae7f9ae6ea3d3fac0d9eb53
marieesss <mje.espinosa@gmail.com>
2023-04-03 14:16:39 +0200

    css

--- web-app/src/pages/Home.jsx
+++ web-app/src/pages/Home.jsx
@@ -1,20 +1,38 @@
 import React, { useState } from 'react';
-import axios from 'axios';
 import Menu from '../components/Menu';
 import Categories from '../components/Categories';
 import Fermiers from '../components/Fermiers';
+import "../css/app.css"
 
 
 const Home = () => {
     return (
-        
-        <div className='container'>
-            <Menu/>
-            <h1>Application</h1>
-            <Categories/>
-            <Fermiers/>
+        <div>
+        <Menu/>
+        <div class="background_home">
+            <div class="row justify-content-between container-home">
+                <div class="col-lg-4 col-sm-12 title-home">
+                    <div class="nouveau">
+                        Nouveau
+                    </div>
+                    <h1>
+                        Livraison Offerte
+                    </h1>
+                    <div class="desc-home">
+                        Recevez votre panier de fruit, directement chez vous dans l'un de nos hubs, gratuitement dès 50 euros d'achat
+                    </div>
+                    <button class="button">
+                        voir nos produits
+                    </button>
+                </div>
+                    <div class="col-6 bg-black title-home">
+                        <div class="row justify-content-center align-self-end">
+                            <img class="img-home" src="https://images.unsplash.com/photo-1579113800032-c38bd7635818?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"/>
+                        </div>
+                    </div>
+            </div>
         </div>
-        
+        </div>
     );
 };
 

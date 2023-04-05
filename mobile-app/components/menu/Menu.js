506ed1ac70ed5e80fbca849c30365491750e298c
marieesss <mje.espinosa@gmail.com>
2023-04-05 12:01:46 +0200

    mobile-app

---
+++ mobile-app/components/menu/Menu.js
@@ -0,0 +1,14 @@
+// import React from 'react'
+// import { createDrawerNavigator } from '@react-navigation/drawer';
+// import About from '../../screens/about/About';
+
+// const Menu = () => {
+// const Drawer= createDrawerNavigator()
+//   return (
+//     <Drawer.Navigator>
+//     <Drawer.Screen name="About" component={About} />
+//   </Drawer.Navigator>
+//   )
+// }
+
+// export default Menu

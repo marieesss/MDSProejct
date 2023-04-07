cb7cc9a1145353da43cf2ae1cd8d42bce169d2c0
marieesss <mje.espinosa@gmail.com>
2023-04-07 14:21:37 +0200

    product details

--- mobile/screens/details/ProductDetail.js
+++ mobile/screens/details/ProductDetail.js
@@ -1,13 +1,14 @@
 
 import React, {useState, useEffect} from 'react'
-import { View,Text, ScrollView, Image} from 'react-native'
-import Header from '../../components/header/Header'
+import { View,Text, ScrollView, Image, FlatList, TouchableOpacity} from 'react-native'
 import axios from 'axios';
+import styles from './productDetail.style'
 
 const ProductDetail = ({ navigation, route }) => {
     const [product, setProduct] = useState({});
     const [fermierId, setFermierId] = useState("");
     const [fermier, setFermier] = useState({});
+    const [products, setProducts] = useState([]);
 
     useEffect(()=>{
         const getProduct = async ()=> {
@@ -45,27 +46,91 @@ const ProductDetail = ({ navigation, route }) => {
         
     }, [fermierId])
 
+    useEffect(()=>{
+      const getProducts = async ()=> {
+        try{
+          const res = await axios.get(`http://10.57.132.20:5000/api/product?fermier=`+fermierId);
+          console.log(res.data)
+          setProducts(res.data)
+
+          
+        }catch(err){
+          console.log(err)
+        }
+      };
+      getProducts();
+     
+    
+      
+  }, [fermierId])
+
+  const renderItemFruit = (data) => {
+    return (
+        <TouchableOpacity style={{height: 150}} onPress={() => {
+          navigation.navigate("ProductDetail", {_id : data.item._id});
+        }}>
+          <View style={styles.product}>
+            <Image source={{uri: data.item.img}}
+              style={{width: 100, height: 100, borderRadius:15}}
+            />
+          </View>
+        </TouchableOpacity>
+    )
+  
+  }
+
   return (
     <View style={{flex: 1}}>
-        <Header/>
         <ScrollView>
-            <View >
-                <Text> {product.title}</Text>
+        <View>
+        <TouchableOpacity onPress={() => navigation.goBack()} >
+        <Image source={require('../../assets/img/back.png')} style={styles.back}/>
+        </TouchableOpacity>
+        </View>
+        <View style={styles.titleRow}>
+  <Text style={styles.welcomeMessage}> {product.title}</Text>
+  <Image source={require('../../assets/img/logo.png')} style={styles.img}/>
+  </View>
+            <View style={styles.row}>
                 <Image source={{uri: product.img}}
-            style={{width: 300, height: 300}}
+            style={{width: 200, height: 200, borderRadius:15}}
             />
-            <Text> {product.price} euros</Text>
-
+              <View>
+                <Text style={styles.text}> Prix : {product.price} euros</Text>
+                <Text style={styles.text}> {product.desc}</Text>
+              </View>
             </View>
+
+            <View style={styles.titleRow}>
+                      <Text style={styles.welcomeMessage}> Autres produits du fermier </Text>
+                </View>
+
+            <FlatList
+                  data={products}
+                  horizontal={true}
+                  renderItem={item => renderItemFruit(item)}
+                  keyExtractor={item => item._id.toString()}
+                  showsVerticalScrollIndicator={false}
+                  showsHorizontalScrollIndicator={false}
+                  style={styles.categorie}
+                />
+
+                <View style={styles.titleRow}>
+                      <Text style={styles.welcomeMessage}>Rencontrez le fermier </Text>
+                </View>
             
-                <Image source={{uri: fermier.img}}
-            style={{width: 300, height: 300}}
-            />
-            <Text> {fermier.name}</Text>
-            <Text> {fermier.desc} euros</Text>
-            <View>
 
-            </View>
+                <View style={styles.row}>
+                    <View>
+                    <Text style={styles.text}> {fermier.name}</Text>
+                    <Text style={styles.text}> {fermier.desc}</Text>
+                    </View>
+                      <Image source={{uri: fermier.img}}
+                    style={{width: 200, height: 200, borderRadius:15}}
+                    />
+                </View>
+            
+           
         </ScrollView>
     </View>
   )

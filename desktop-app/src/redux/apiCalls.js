ada45e4fdbad51820064aced9219f198168944af
marieesss <mje.espinosa@gmail.com>
2023-05-13 12:47:50 +0200

    fix bug

--- desktop-app/src/redux/apiCalls.js
+++ desktop-app/src/redux/apiCalls.js
@@ -1,11 +1,23 @@
 import { loginFailure, loginStart, loginSuccess } from "./userRedux";
 import { publicRequest } from "../requestMethods";
+import axios from "axios";
+
 import { useNavigate } from "react-router-dom";
 
 export const login = async (dispatch, user) => {
   dispatch(loginStart());
   try {
-    const res = await publicRequest.post("/auth/login", user);
+    const res = await axios.post(
+      "http://141.94.244.226:5000/api/auth/login",
+      user,
+      {
+        headers: {
+          "Access-Control-Allow-Headers": "*",
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "*"
+        }
+      }
+    );
     dispatch(loginSuccess(res.data));
    
 
